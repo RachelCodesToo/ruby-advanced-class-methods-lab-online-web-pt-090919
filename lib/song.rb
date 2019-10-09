67569d8c5497eb6d776167d8d34fe8388c9d1fcf
Anne Fausto <a.r.fausto@gmail.com>
2019-10-09 05:26:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
   end 
   
   def self.create_from_file_name 
-     array = filename.split(" - ")
+    array = filename.split(" - ")
     artist_name = array[0]
     song_name = array[1].gsub(".mp3", "")
     song = create
